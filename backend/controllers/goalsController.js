b30babfdf5af91565f533946e2ddc25f885aaab2
Satheesh <shakthi2704@gmail.com>
2023-10-15 17:38:20 +0530

    Goals_CURD_testing

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,39 +1,63 @@
 import asyncHandler from "express-async-handler"
-
+import Goal from "../models/goalModel.js"
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = asyncHandler ((req,res) =>{
-    res.status(200).json({Message:"get goal!@"})
+const getGoals = asyncHandler (async(req,res) =>{
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 // @desc    Set goal
 // @route   POST /api/goals
 // @access  Private
-const setGoal = asyncHandler((req,res) =>{
-    console.log(req.body)
-    if(!req.body.text){
+const setGoal = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
         res.status(400)
-        throw new Error('Please add text')
+        throw new Error('Please add text');
     }
-  
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+ 
+    res.status(200).json(goal)
 })
+
 
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = asyncHandler((req,res) =>{
-    res.status(200).json({Message:`update ${req.params.id}`})
+const updateGoal = asyncHandler(async(req,res) =>{
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      })
+      res.status(200).json(updatedGoal)
 })
 
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = asyncHandler((req,res) =>{
-    res.status(200).json({Message:`delete ${req.params.id}`})
+const deleteGoal = asyncHandler(async(req,res) =>{
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+    res.status(400)
+    throw new Error('Goal not found')
+    }
+
+    await Goal.deleteOne({ _id: goal._id });
+
+  res.status(200).json({ id: req.params.id })
 })
 
 
