2cb1def0c46103e83a270b148ae3f2ac08bb0a46
Satheesh <shakthi2704@gmail.com>
2023-10-18 18:46:38 +0530

    vite_config_1.3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://merngoals.vercel.app/",
+        target: "https://merngoals-kmeikiwfh-shakthi2704.vercel.app/",
         changeOrigin: true,
       },
     },
